58404388e09c806525fcb82deb4999cb22686e78
minsungcho866 <chom866@newschool.edu>
2024-03-15 19:50:15 -0400

    lab project 4

---
+++ lab/projects/04_collection-object/assets/data.js
@@ -0,0 +1,23 @@
+// An array of objects representing my collection:
+var data = [
+    {
+        location: 'H MART',
+        item: 'Asian grocery, bag',
+        price: 12.53,
+    },
+    {
+        location: 'GAMMEEOK',
+        item: 'Bossam, Bindaetteok, Terra, Saero, Abai Sundae',
+        price: 140.79,
+    },
+    {
+        location: 'Sala Thai',
+        item: 'Tom Yum Koong, Pad Thai, Thai fried Rice, Curry Puffs',
+        price: 67.50,
+    },
+    {
+        location: 'New Mizu Sushi',
+        item: 'Philadelphia Roll, Soda, Tricolor Cuchi Dinner',
+        price: 38.54,
+    },
+];
