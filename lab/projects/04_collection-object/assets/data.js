36de92de705681cbf76f74b3e023513d348442c3
minsungcho866 <chom866@newschool.edu>
2024-03-15 20:37:51 -0400

    Update data.js

--- lab/projects/04_collection-object/assets/data.js
+++ lab/projects/04_collection-object/assets/data.js
@@ -25,4 +25,105 @@ var data = [
         item: 'BBQ Mix Box, Kara-age',
         price: 32.06,
     },
+    {
+        location: 'Taco Bell',
+        item: 'Cheesy Double Beef Burrito, Nachos BellGrande, Soft Taco, Cheese Roll Up',
+        price: 21.54,
+    },
+    {
+        location: 'Skyline Grourment Deli',
+        item: 'Slice of Orea Cheesecake, Iced Coffee, Chop Cheese, Bacon, Egg, and Cheese Breakfest Sandwich',
+        price: 18.67,
+    },
+    {
+        location: 'FIFTYLAN Koreantwon',
+        item: 'Brown Sugar Milk Tea',
+        price: 19.67,
+    },
+    {
+        location: 'And Pizza',
+        item: 'Dirty Birdless',
+        price: 23.21,
+    },
+    {
+        location: 'Dim Sum Sam',
+        item: 'Crispy Pork Belly Rice, Pork Belly Sandwich',
+        price: 21.77,
+    },
+    {
+        location: 'Panda Express',
+        item: 'Plate',
+        price: 12.23,
+    },
+    {
+        location: 'Zurutto Ramen',
+        item: 'Zurutto Deluxe',
+        price: 26.45,
+    },
+    {
+        location: '7th Street Burger',
+        item: 'Double Cheeseburger',
+        price: 16.86,
+    },
+    {
+        location: 'Don Poke',
+        item: 'Don Poke Bowl',
+        price: 28.09,
+    },
+    {
+        location: 'Joes Pizza',
+        item: 'Plain Cheese Pizza',
+        price: 38.11,
+    },
+
+    {
+        location: 'Hyo Dong Gak',
+        item: 'Noodles with Seafood Brown Sauce',
+        price: 22.97,
+    },
+    {
+        location: 'Soju Haus',
+        item: 'Braised Pork Belly Kimchi, Bowl of Rice',
+        price: 26.13,
+    },
+    {
+        location: 'Mala Project',
+        item: 'Mala Dry Pot',
+        price: 59.85,
+    },
+    {
+        location: 'ROYCE CHOCOLATE',
+        item: 'Nama Chocolate "Mild Cacao"',
+        price: 26.54,
+    },
+    {
+        location: 'Sakura Sushi and Ramen',
+        item: 'Fried wonton, Sushi and Sashimi For 2',
+        price: 87.18,
+    },
+    {
+        location: 'WingBox',
+        item: 'Nashville Hot Honey Rub Wings',
+        price: 31.93,
+    },
+    {
+        location: 'Krispy Kreme Doughnuts',
+        item: '6 count Original Glazed Doughnuts',
+        price: 19.16,
+    },
+    {
+        location: 'Kyuramen',
+        item: 'Tonkotsu Shouyu Ramen',
+        price: 47.69,
+    },
+    {
+        location: 'Khao Hom Thai',
+        item: 'Large Tom Yum Soup, Pad Tahi Noodle, Yellow Curry, Thai Fried Rice, Khao Hom Sweet and Sour',
+        price: 104.65,
+    },
+    {
+        location: 'Daves Hot Chicken',
+        item: 'Daves 4:slider with Fries',
+        price: 12.81,
+    },
 ];
