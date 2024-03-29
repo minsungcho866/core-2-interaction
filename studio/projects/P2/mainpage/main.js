47463953a6b3db750661837ae20ed9b42a1ed888
minsungcho866 <chom866@newschool.edu>
2024-03-28 21:12:22 -0400

    Update main.js

--- studio/projects/P2/mainpage/main.js
+++ studio/projects/P2/mainpage/main.js
@@ -1,70 +1,47 @@
-document.getElementById('high').addEventListener("click", function() {
-    var low = document.querySelectorAll('.low');
-    var medium = document.querySelectorAll('.medium');
-    
-    if (this.checked) {
-        // If the checkbox is checked, hide the images
-        low.forEach(function(element) {
-            element.style.display = "none";
-        });
-        medium.forEach(function(element) {
-            element.style.display = "none";
-        });
-    } else {
-        // If the checkbox is unchecked, show the images
-        low.forEach(function(element) {
-            element.style.display = "block";
-        });
-        medium.forEach(function(element) {
-            element.style.display = "block";
-        });
-    }
-});
-// -----------------------------
-document.getElementById('medium').addEventListener("click", function() {
-    var low = document.querySelectorAll('.low');
-    var high = document.querySelectorAll('.high');
-    
-    if (this.checked) {
-        // If the checkbox is checked, hide the images
-        low.forEach(function(element) {
-            element.style.display = "none";
-        });
-        high.forEach(function(element) {
-            element.style.display = "none";
-        });
-    } else {
-        // If the checkbox is unchecked, show the images
-        low.forEach(function(element) {
-            element.style.display = "block";
-        });
-        high.forEach(function(element) {
-            element.style.display = "block";
-        });
-    }
-});
-// -----------------------------
-document.getElementById('low').addEventListener("click", function() {
-    var medium = document.querySelectorAll('.medium');
-    var high = document.querySelectorAll('.high');
-    
-    if (this.checked) {
-        // If the checkbox is checked, hide the images
-        high.forEach(function(element) {
-            element.style.display = "none";
-        });
-        medium.forEach(function(element) {
-            element.style.display = "none";
-        });
-    } else {
-        // If the checkbox is unchecked, show the images
-        high.forEach(function(element) {
-            element.style.display = "block";
-        });
-        medium.forEach(function(element) {
-            element.style.display = "block";
-        });
-    }
+var checkboxes = document.querySelectorAll('input[type="checkbox"]');
+
+checkboxes.forEach(function(checkbox) {
+    checkbox.addEventListener("click", function() {
+        var checkedCheckboxes = document.querySelectorAll('input[type="checkbox"]:checked');
+        
+        var low = document.querySelectorAll('.low');
+        var medium = document.querySelectorAll('.medium');
+        var high = document.querySelectorAll('.high');
+        
+        if (checkedCheckboxes.length === 0) {
+            low.forEach(function(element) {
+                element.style.display = "block";
+            });
+            medium.forEach(function(element) {
+                element.style.display = "block";
+            });
+            high.forEach(function(element) {
+                element.style.display = "block";
+            });
+        } else {
+            low.forEach(function(element) {
+                if (Array.from(checkedCheckboxes).some(cb => cb.id === 'low')) {
+                    element.style.display = "block";
+                } else {
+                    element.style.display = "none";
+                }
+            });
+            medium.forEach(function(element) {
+                if (Array.from(checkedCheckboxes).some(cb => cb.id === 'medium')) {
+                    element.style.display = "block";
+                } else {
+                    element.style.display = "none";
+                }
+            });
+            high.forEach(function(element) {
+                if (Array.from(checkedCheckboxes).some(cb => cb.id === 'high')) {
+                    element.style.display = "block";
+                } else {
+                    element.style.display = "none";
+                }
+            });
+        }
+    });
 });
 
 //---------------------------------
